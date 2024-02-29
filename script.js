b46e797140b6b5c791c4773ed46ddac1aa7b7223
Pedro Molinaro <plmolinaro12@gmail.com>
2024-02-28 22:00:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area-1', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000
+});
+
+sr.reveal('.area-2', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000
+});
+
+sr.reveal('.area-3', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000
+});
